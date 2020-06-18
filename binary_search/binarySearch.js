23cc2d33e61202fb485edbbb8421d0cfff9d2d99
David Lacedonia <davidlacedonia@gmail.com>
2020-06-18 20:21:47 +0200

    binary search doc

--- binary_search/binarySearch.js
+++ binary_search/binarySearch.js
@@ -4,7 +4,13 @@ const { expect } = require("../expect");
 const TARGET = 7;
 
 /**
- * Binary Search O(log n)
+ * Binary Search O(log n).
+ *
+ * Assigns middle index to pivot
+ * If the value on index 'pivot' equals the target, returns the index
+ * If the value is lesser, move the left index to pivot + 1
+ * If the value is greater, move the right index to pivot - 1
+ * Moving the left/right indexes, will reduce the search in halves.
  */
 function binarySearch(array, target) {
   let left = 0;

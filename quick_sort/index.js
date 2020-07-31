c201e7093e1eeb1342ec29ff655fe30913b5adcc
David Lacedonia <davidlacedonia@gmail.com>
2020-07-31 09:14:54 +0200

    Quick sort

---
+++ quick_sort/index.js
@@ -0,0 +1,42 @@
+const { expect } = require("../expect");
+const { UNORDERED_ARRAY, ORDERED_ARRAY } = require("../data");
+
+function partition(array, min, max) {
+  const pivot = array[max];
+  let i = min - 1;
+
+  for (let j = min; j <= max - 1; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      let aux = array[i];
+      array[i] = array[j];
+      array[j] = aux;
+    }
+  }
+
+  let aux = array[i + 1];
+  array[i + 1] = array[max];
+  array[max] = aux;
+
+  return i + 1;
+}
+
+/**
+ * Quick sort O(n log n).
+ *
+ * Cuts the array on half every time (using a pivot),
+ * and orders the left part of the pivot, and positions the pivot
+ * in the correct pisition
+ */
+function quickSort(array, min, max) {
+  if (min < max) {
+    const pivot = partition(array, min, max);
+    quickSort(array, min, pivot - 1);
+    quickSort(array, pivot + 1, max);
+  }
+
+  return array;
+}
+
+const result = quickSort(UNORDERED_ARRAY, 0, UNORDERED_ARRAY.length - 1);
+expect(result).toBe(ORDERED_ARRAY);

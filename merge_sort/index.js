dd5e0ff5189a3bcc07faa7b251b44177973c7bdd
David Lacedonia <davidlacedonia@gmail.com>
2020-08-06 18:49:38 +0200

    merge sort

---
+++ merge_sort/index.js
@@ -0,0 +1,39 @@
+const { expect } = require("../expect");
+const { UNORDERED_ARRAY, ORDERED_ARRAY } = require("../data");
+
+const sortHalves = (left, right) => {
+  let sorted = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length || j < right.length) {
+    if (left[i] <= right[j] || !right[j]) {
+      sorted.push(left[i]);
+      i++;
+    } else {
+      sorted.push(right[j]);
+      j++;
+    }
+  }
+
+  return sorted;
+};
+
+/**
+ * Merge sort O(n log n).
+ *
+ * Divide array in halves recursively,
+ * sorting and merging left and right.
+ */
+function mergeSort(array) {
+  if (array.length < 2) return array;
+
+  const middle = Math.ceil(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle, array.length);
+
+  return sortHalves(mergeSort(left), mergeSort(right));
+}
+
+const result = mergeSort(UNORDERED_ARRAY);
+expect(result).toBe(ORDERED_ARRAY);

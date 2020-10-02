c3e7a03f57bb5f13df037f904400cb81ae19bb49
David Lacedonia <davidlacedonia@gmail.com>
2020-10-02 15:00:25 +0200

    feature: LRU cache

---
+++ src/miscellaneous/lru_cache/index.js
@@ -0,0 +1,41 @@
+/**
+ * LRU Cache.
+ *
+ * Least recent used cache.
+ *
+ * @author David Lacedonia <davidlacedonia@gmail.com>
+ *
+ * @param {number} capacity Limit of elements
+ *
+ * @return {object} get and put LRU methods
+ */
+function LRUCache(capacity) {
+  let values = new Array(capacity);
+
+  return {
+    get(value) {
+      const found = values.find((item) => item === value);
+
+      if (found) {
+        values = values.filter((v) => v !== found);
+        values.push(value);
+      }
+
+      return found || -1;
+    },
+
+    put(value) {
+      if (values.length >= capacity) {
+        if (values.find((item) => item === value)) {
+          values = values?.filter((v) => v !== value);
+        } else {
+          values.splice(0, 1);
+        }
+      }
+
+      values.push(value);
+    },
+  };
+}
+
+export default LRUCache;
---
+++ src/miscellaneous/lru_cache/index.test.js
@@ -0,0 +1,32 @@
+import LRUCache from "./index";
+
+test("LRU cache access first element", () => {
+  const cache = LRUCache(2);
+  cache.put(1);
+  cache.put(2);
+  const result = cache.get(1);
+  expect(result).toStrictEqual(1);
+});
+
+test("LRU cache access removed item", () => {
+  const cache = LRUCache(2);
+  cache.put(1);
+  cache.put(2);
+  cache.get(1);
+  cache.put(3);
+  const result = cache.get(2);
+  expect(result).toStrictEqual(-1);
+});
+
+test("LRU cache access item after another removal", () => {
+  const cache = LRUCache(2);
+  cache.put(1);
+  cache.put(2);
+  cache.get(1);
+  cache.put(3);
+  cache.get(2);
+  cache.put(4);
+  cache.get(1);
+  const result = cache.get(3);
+  expect(result).toStrictEqual(3);
+});

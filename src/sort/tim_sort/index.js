5b74290639ecdd4e2b6fa4b5f47d5c11bed11790
David Lacedonia <davidlacedonia@gmail.com>
2021-01-20 08:50:38 +0100

    feature: tim sort refactor

--- src/sort/tim_sort/index.js
+++ src/sort/tim_sort/index.js
@@ -1,19 +1,27 @@
-import insertionSort from "../insertion_sort";
-import mergeSort from "../merge_sort";
+import insertionSort from '../insertion_sort';
+import mergeSort from '../merge_sort';
 
 /**
  * Tim sort O(n log n).
  *
- * Divide array into blocks (runs).
- * Sort every run using insertion sort.
- * Merge sorted runs one by one using merge sort.
+ * Divide array into blocks (runs),
+ * sorts every run using insertion sort and then,
+ * merge sorted runs one by one using merge sort.
+ *
+ * @author David Lacedonia <davidlacedonia@gmail.com>
+ *
+ * @param {array} array unsorted elements
+ * @param {number} RUN_SIZE size un runs
+ *
+ * @return {array} sorted elements
  */
-function timSort(array) {
-  const run = 3;
+function timSort(array, RUN_SIZE = 3) {
   let blocks = [];
   let mergedBlocks = [];
 
-  for (let i = 0; i <= array.length; i += run) {
+  if (!Array.isArray(array)) return [];
+
+  for (let i = 0; i <= array.length; i += RUN_SIZE) {
     const block = array.slice(i, i + 3);
     const sortedBlock = insertionSort(block);
     blocks.push(sortedBlock);
--- src/sort/tim_sort/index.test.js
+++ src/sort/tim_sort/index.test.js
@@ -1,7 +1,19 @@
-import { UNORDERED_ARRAY, ORDERED_ARRAY } from "../../data";
-import timSort from "./index";
+import { UNORDERED_ARRAY, ORDERED_ARRAY } from '../../data';
+import { randomNumbers } from '../../utils';
+import timSort from './index';
 
-test("tim sort", () => {
+test('tim sort', () => {
   const result = timSort(UNORDERED_ARRAY);
   expect(result).toStrictEqual(ORDERED_ARRAY);
+});
+
+test('tim sort with random numbers', () => {
+  const unsortedNumbers = randomNumbers(10, 500);
+  const sortedNumbers = unsortedNumbers.sort((a, b) => a - b);
+  expect(timSort(unsortedNumbers)).toStrictEqual(sortedNumbers);
+});
+
+test('tim sort with wrong types', () => {
+  const result = timSort('wrong type');
+  expect(result).toStrictEqual([]);
 });

af36244fcb133f6e0e2de6e853dfa57764d5d794
David Lacedonia <davidlacedonia@gmail.com>
2021-01-26 08:35:54 +0100

    refactor: jump search

--- src/search/jump_search/index.js
+++ src/search/jump_search/index.js
@@ -1,32 +1,36 @@
 /**
  * Jump search O(sqrt(n))
  *
- * @param {object} array Array of values
- * @param {number} element Element to search
+ * Tries to check fewer elements by jumping ahead fixed steps.
+ * Jump step is the square root of the number of elements.
+ * If the values is greater than the element,
+ * do a linear search backwards until found.
+ *
+ * @author David Lacedonia <davidlacedonia@gmail.com>
+ *
+ * @param {array} array sorted values
+ * @param {number} el Element to search
  *
  * @return {number} Index of element
  */
-function jumpSearch(array, element) {
-  // jump is the square root of the amount of elements
+function jumpSearch(array = [], el) {
   const jump = Math.floor(Math.sqrt(array.length));
 
-  // loop through the array, every "jump" size
+  if (!Array.isArray(array)) return null;
+
   for (let i = 0; i < array.length; i += jump) {
-    // return index if found
-    if (array[i] === element) {
+    if (array[i] === el) {
       return i;
-    } else if (array[i] > element) {
-      // if value is greater than the element
-      // do a linear search backwards until found
+    } else if (array[i] > el) {
       for (let j = i; j > i - jump; j--) {
-        if (array[j] === element) {
+        if (array[j] === el) {
           return j;
         }
       }
     }
   }
 
-  return -1;
+  return null;
 }
 
 export default jumpSearch;
--- src/search/jump_search/index.test.js
+++ src/search/jump_search/index.test.js
@@ -1,7 +1,12 @@
-import { ORDERED_ARRAY } from "../../data";
-import jumpSearch from "./index";
+import { ORDERED_ARRAY } from '../../data';
+import jumpSearch from './index';
 
-test("jump search", () => {
+test('jump search', () => {
   const result = jumpSearch(ORDERED_ARRAY, ORDERED_ARRAY[6]);
   expect(result).toStrictEqual(6);
+});
+
+test('jump search with wrong values', () => {
+  const result = jumpSearch('wrong value');
+  expect(result).toStrictEqual(null);
 });

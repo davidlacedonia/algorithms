6243f1a45c208094aec4be53aeeafaf8fe624a78
David <davidlacedonia@gmail.com>
2021-11-24 09:39:50 +0100

    feat: serialize and deserialize n ary tree

---
+++ src/structures/n_ary_tree/index.js
@@ -0,0 +1,91 @@
+function Node(val, children = []) {
+  this.val = val;
+  this.children = children;
+}
+
+export default class Codec {
+
+  constructor() {}
+
+  /** 
+   * Encodes a tree to a single string.
+   * 
+   * @param {Node|null} root
+   * @return {string}
+   */
+  serialize = function(root) {
+      let acc = []
+      let flatten = []
+      
+      const _internal = (root, level = 0, last = true) => {
+          if (!root) return
+          acc[level] = acc[level] ? [...acc[level], root.val] : [root.val]
+      
+          if (last) acc[level] = [...acc[level], 'null']
+          
+          if (!root.children.length) {
+              acc[level + 1] = acc[level + 1] ? [...acc[level + 1], 'null'] : ['null']
+          }
+
+          for (let i = 0; i < root.children.length; i++) {
+              _internal(root.children[i], level + 1, i === root.children.length - 1)
+          }
+      }
+      
+      _internal(root)
+      for (let arr of acc) {
+          flatten.push(...arr)
+      }
+      
+      
+      let nullable = !flatten[flatten.length - 1]
+      for (let i = flatten.length - 1; i >= 0 && nullable; i--) {
+          if (flatten[i] !== 'null') {
+              nullable = false
+              flatten.length = i + 1
+          }
+      }
+      
+      const _stringifiedFlatten = '[' + flatten.join(',') + ']'
+      return _stringifiedFlatten
+  };
+
+  /** 
+   * Decodes your encoded data to tree.
+   * 
+   * @param {string} data 
+   * @return {Node|null}
+   */
+  deserialize = function(data) {
+      const values = data.slice(1, data.length - 1).split(',')
+      let head = new Node(values[0], [])
+      
+      const _process = (children, initial) => {
+          if (!children?.length || initial > values.length) return
+              
+          let j = initial
+          let _children = []
+          
+          for (let i = 0; i < children.length; i++) {
+              const curr = children[i]
+              
+              if (values[j] === 'null' || !values[j]) {
+                  j++
+              } else {
+                  while (values[j] !== 'null' && values[j]) {
+                      const node = new Node(values[j], [])
+                      curr.children.push(node)
+                      _children.push(node)
+                      j++
+                  }
+                  j++
+              }
+          }
+          
+          _process(_children, j)
+      }
+      
+      _process([head], 2)
+      return head
+  };
+}
---
+++ src/structures/n_ary_tree/index.test.js
@@ -0,0 +1,14 @@
+import Codec from './index'
+
+let codec
+
+beforeAll(() => {
+  codec = new Codec()
+  console.log(codec)
+})
+
+test('Deserialize N ary Tree', () => {
+  const root = '[1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14,null]'
+  const deserialized = codec.deserialize(root);
+  expect(deserialized.val).toEqual('1')
+})

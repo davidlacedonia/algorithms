510da58a61b07c122c5d969f9944a554c731b0c5
David Lacedonia <davidlacedonia@gmail.com>
2024-06-19 08:55:38 +0200

    feat(binary_tree): sum root to leaf numbers

---
+++ src/binary_tree/challenges/sumRootToLeaf.test.js
@@ -0,0 +1,35 @@
+import sumRootToLeaf from './sumRootToLeaf.js';
+
+test('Sum root to leaf numbers', () => {
+  const result = sumRootToLeaf({
+    val: 1,
+    left: {
+      val: 2,
+    },
+    right: {
+      val: 3,
+    },
+  });
+
+  expect(result).toStrictEqual(25);
+});
+
+test('Sum root to leaf numbers', () => {
+  const result = sumRootToLeaf({
+    val: 4,
+    left: {
+      val: 9,
+      left: {
+        val: 5,
+      },
+      right: {
+        val: 1,
+      },
+    },
+    right: {
+      val: 0,
+    },
+  });
+
+  expect(result).toStrictEqual(1026);
+});

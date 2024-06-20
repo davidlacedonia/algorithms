35dddd973779233e1a4420ee25e3feb54b9a04c4
David Lacedonia <davidlacedonia@gmail.com>
2024-06-20 08:41:23 +0200

    feat(binary_tree): invert tree

---
+++ src/binary_tree/challenges/invert.js
@@ -0,0 +1,11 @@
+export function invertTree(root) {
+  if (!root) return root;
+  invertTree(root.left);
+  invertTree(root.right);
+
+  const aux = root.right;
+  root.right = root.left;
+  root.left = aux;
+
+  return root;
+}

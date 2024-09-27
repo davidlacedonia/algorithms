25252a7affb9bd33200f5457d17a96615e51d2e6
David Lacedonia <davidlacedonia@gmail.com>
2024-09-27 08:38:09 +0200

    feat(common_problems): kadane circular

--- src/common_problems/kadane/index.js
+++ src/common_problems/kadane/index.js
@@ -10,3 +10,21 @@ export function maxSubArray(nums) {
 
     return max;
 }
+
+export function maxSubarraySumCircular(nums) {
+    let max = nums[0],
+        min = nums[0],
+        maxCurr = 0,
+        minCurr = 0,
+        total = 0;
+
+    for (let num of nums) {
+        maxCurr = Math.max(maxCurr + num, num);
+        max = Math.max(maxCurr, max);
+        minCurr = Math.min(minCurr + num, num);
+        min = Math.min(minCurr, min);
+        total += num;
+    }
+
+    return max > 0 ? Math.max(max, total - min) : max;
+}
--- src/common_problems/kadane/index.test.js
+++ src/common_problems/kadane/index.test.js
@@ -1,6 +1,11 @@
-import { maxSubArray } from '.';
+import { maxSubArray, maxSubarraySumCircular } from '.';
 
 test("Kadane's max sub array", () => {
     const output = maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]);
     expect(output).toEqual(6);
+});
+
+test("Kadane's max sub array", () => {
+    const output = maxSubarraySumCircular([5, -3, 5]);
+    expect(output).toEqual(10);
 });
